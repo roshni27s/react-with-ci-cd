dbba8203425050842917b7b95f6b1632ae272f87
Roshni Singh <roshni.rx@gmail.com>
2023-04-16 19:50:47 +0530

    gh-pages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>Testing ci cd pipeline with github actions</h1>
+          <h1>Test ci cd pipeline with github actions</h1>
         </p>
         <a
           className="App-link"
