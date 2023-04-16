9128a146ee05bd590857ac63e1536ae2d41c64de
Roshni Singh <roshni.rx@gmail.com>
2023-04-16 20:31:15 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>Test ci cd pipeline with github actions</h1>
+          <h1>ci cd pipeline with github actions</h1>
         </p>
         <a
           className="App-link"
