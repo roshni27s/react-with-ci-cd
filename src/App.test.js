b2f9ca207741b2a1398114bf359c9652664f6e3c
Roshni Singh <roshni.rx@gmail.com>
2023-04-18 00:54:30 +0530

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,10 @@
 import { render, screen } from '@testing-library/react';
+import localStorageMock from './localStorageMock';
 import App from './App';
+
+beforeEach(() => {
+  global.localStorage = localStorageMock;
+});
 
 test('renders learn react link', () => {
   render(<App />);
