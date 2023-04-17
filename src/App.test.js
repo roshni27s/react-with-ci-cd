33c53a2d831f778d78ffb3ddec0babaa8b81f214
Roshni Singh <roshni.rx@gmail.com>
2023-04-17 12:36:04 +0530

    test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const checkBudget = screen.getByText(/Budgets/i);
-  expect(checkBudget).toEqual('Budgets');
+  const checkBudget = screen.getByText(/Budgets/);
+  expect(checkBudget.textContent).toEqual('Budgets');
 });
