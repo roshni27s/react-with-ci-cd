b9334bfdcbe3dbd71ca257d8d4ffbcb9b5108d19
Roshni Singh <roshni.rx@gmail.com>
2023-04-17 12:32:07 +0530

    start code test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const checkBudget = screen.getByText(/Budgets/i);
+  expect(checkBudget).toEqual('Budgets');
 });
